9c16e19e12d541ec0192fae9516007ebc2b61e97
Sam Nicholls <samstudio8@gmail.com>
2020-03-29 13:15:28 +0100

    0.0.7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="swell",
-    version="0.0.6",
+    version="0.0.7",
     url="https://github.com/samstudio8/swell",
 
     description="A command line tool for surfing your coverage",
