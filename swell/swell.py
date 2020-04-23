9cf0a43d7a88b398d04dec82e911ca7d14b403b4
Sam Nicholls <samstudio8@gmail.com>
2020-04-23 22:56:23 +0100

    experiment with aggressive clips

--- swell/swell.py
+++ swell/swell.py
@@ -52,12 +52,12 @@ def load_scheme(scheme_bed):
         # Clip the start of this window to the end of the last window
         # (if there is a last window)
         if (tile_i0 - 1) >= 0:
-            tile[1] = l_tiles[tile_i0 - 1][2][3] + 1
+            tile[1] = l_tiles[tile_i0 - 1][2][3] + 5
 
         # Clip the end of this window to the start of the next window
         # (if there is a next window)
         if (tile_i0 + 1) < len(l_tiles):
-            tile[2] = l_tiles[tile_i0 + 1][2][0] - 1
+            tile[2] = l_tiles[tile_i0 + 1][2][0] - 5
 
         new_tiles.append((tile_t[0], tile_t[1], tile))
 
